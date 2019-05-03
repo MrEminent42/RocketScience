8d7c8d756c86d422e4e5ef8851508035d5eb3157
MrEminent42 <MrEminent42@users.noreply.github.com>
2019-05-03 15:23:18 +0800

    Basic Main & Rocket classes
    Updated Main.java with main method.
    
    Created Pos class and Rocket class containing a Pos location.

---
+++ Pos.java
@@ -0,0 +1,18 @@
+public class Pos {
+    
+    double x, y;
+    
+    public Pos(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double getX() {
+        return x;
+    }
+    
+    public double getY() {
+        return y;
+    }
+    
+}
---
+++ Rocket.java
@@ -0,0 +1,8 @@
+public class Rocket {
+    
+    Pos pos;
+    
+    public Rocket() {
+        this.pos = new Pos(0,0);
+    }
+}
