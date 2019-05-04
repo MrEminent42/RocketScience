92cc9b3769637b282d951cf817487e9dae5b3716
MrEminent42 <MrEminent42@users.noreply.github.com>
2019-05-04 16:41:49 +0800

    Vector class & rocket movement
    Created the new Vector class to replace preivous Pos class.
    Added basic rocket movement: gravity (1 unit per refresh at the moment) and acceleration/velocity/position.

--- Rocket.java
+++ Rocket.java
@@ -1,8 +1,20 @@
 public class Rocket {
     
-    Pos pos;
+    Vector pos;
+    Vector accl;
+    Vector vel;
     
     public Rocket() {
-        this.pos = new Pos(0,0);
+        this.pos = new Vector(0,0);
+        this.accl = new Vector(0,0);
+        this.vel = new Vector(0,0);
+    }
+    
+    public void update() {
+        this.accl.add(new Vector(0,RocketGame.GRAVITY));
+        
+        this.vel.add(accl);
+        this.pos.add(vel);
+        this.accl = new Vector(0,0);
     }
 }
--- RocketGame.java
+++ RocketGame.java
@@ -1,10 +1,16 @@
 public class RocketGame {
     
+    public static final float GRAVITY = -1;
+    
     //RocketScreen screen;
     Rocket rocket;
     
     public RocketGame() {
         this.rocket = new Rocket();
+    }
+    
+    public void update() {
+        rocket.update();
     }
     
 }
